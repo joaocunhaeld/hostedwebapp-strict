33427b1e43b562e92f364c2fb9687c49c650519c
joaocunhaeld <joao.cunha@eldorado.org.br>
2015-06-19 04:46:39 -0700

    initial solution version

---
+++ App2/js/default.js
@@ -0,0 +1,203 @@
+﻿function setWinJS() {
+    var winjsfile = document.createElement("script");
+    winjsfile.type = "application/javascript";
+    winjsfile.src = "ms-appx-web:///WinJS/js/WinJS.js";
+    document.body.appendChild(winjsfile);
+
+    var cssFile = document.createElement("link");
+    cssFile.rel = "stylesheet";
+    cssFile.type = "text/css";
+    cssFile.href = "ms-appx-web:///WinJS/css/ui-light.css";
+    cssFile.media = "all";
+    document.head.appendChild(cssFile);
+    
+    winjsfile.onload = function (args) {
+        WinJS.UI.Animation.fadeOut(WinJS.Utilities.query(".loadingContent").get(0)).then(function (args) {
+            WinJS.Utilities.query(".loadingContent").get(0).style.zIndex = -1;
+        });
+
+        window.onloadstart = (function (args) {
+            WinJS.UI.Animation.fadeIn(WinJS.Utilities.query(".loadingContent").get(0)).then(function (args) {
+                WinJS.Utilities.query(".loadingContent").get(0).style.zIndex = 999;
+            });
+        });
+
+        window.onload = (function (args) {
+            WinJS.UI.Animation.fadeOut(WinJS.Utilities.query(".loadingContent").get(0)).then(function (args) {
+                WinJS.Utilities.query(".loadingContent").get(0).style.zIndex = -1;
+            });
+        });
+
+        window.onerror = (function (args) {
+            WinJS.UI.Animation.fadeOut(WinJS.Utilities.query(".loadingContent").get(0)).then(function (args) {
+                WinJS.Utilities.query(".loadingContent").get(0).style.zIndex = -1;
+            });
+        });
+
+        addAppBar();
+    };
+
+    document.body.role = "application";
+    document.body.style.overflowX = "auto";    
+}
+
+function addAppBar() {
+    var pinDiv = document.createElement("div");
+    pinDiv.id = "pinUnpinFromAppbar";
+
+    var pinButton = document.createElement("button");
+
+    pinDiv.appendChild(pinButton);
+    document.body.appendChild(pinDiv);
+
+    var appBarCommandPin = new WinJS.UI.AppBarCommand(pinButton);
+    appBarCommandPin.id = "commandButton";
+    appBarCommandPin.section = "global";
+    appBarCommandPin.icon = "pin";
+
+    var appBar = new WinJS.UI.AppBar(pinDiv);
+
+    setAppbarButton();
+
+    appBar.disabled = false;
+    document.getElementById("commandButton").addEventListener("click", appbarButtonClicked, false);
+
+    WinJS.UI.processAll();
+}
+
+function setAppbarButton() {
+    var appBar = document.getElementById("pinUnpinFromAppbar");
+    var commandButton = document.getElementById("commandButton").winControl;
+
+    if (Windows.UI.StartScreen.SecondaryTile.exists(getAppbarTileId())) {
+        commandButton.label = "Unpin from start";
+        commandButton.icon = "unpin";
+    }
+    else {
+        commandButton.label = "Pin to start";
+        commandButton.icon = "pin";
+    }
+
+    appBar.winControl.sticky = false;
+}
+
+function appbarButtonClicked() {
+    var appBar = document.getElementById("pinUnpinFromAppbar");
+    appBar.winControl.sticky = true;
+
+    var commandButton = document.getElementById("commandButton");
+
+    if (WinJS.UI.AppBarIcon.unpin == commandButton.winControl.icon) {
+        unpinByElementAsync(commandButton, getAppbarTileId()).done(function (isDeleted) {
+            if (isDeleted) {
+                WinJS.log && WinJS.log("Secondary tile was successfully unpinned.", "sample", "status");
+            }
+            else {
+                WinJS.log && WinJS.log("Secondary tile was not unpinned.", "sample", "error");
+            }
+
+            setAppbarButton();
+        });
+    }
+    else {
+        pinByElementAsync(commandButton, getAppbarTileId(), appbarTileUri).done(function (isCreated) {
+            if (isCreated) {
+                WinJS.log && WinJS.log("Secondary tile was successfully pinned.", "sample", "status");
+            }
+            else {
+                WinJS.log && WinJS.log("Secondary tile was not pinned.", "sample", "error");
+            }
+
+            setAppbarButton();
+        });
+    }
+}
+
+function pinByElementAsync(element, newTileId, newTileDisplayName) {
+    var square150X150Logo = new Windows.Foundation.Uri("ms-appx:///images/logo.scale-100.png");
+    var square30x30Logo = new Windows.Foundation.Uri("ms-appx:///images/smalllogo.scale-100.png");
+
+    var currentTime = new Date();
+    var newTileDesiredSize = Windows.UI.StartScreen.TileSize.square150x150;
+
+    var tile = new Windows.UI.StartScreen.SecondaryTile(getAppbarTileId(), newTileDisplayName, appbarTileUri, square150X150Logo, newTileDesiredSize);
+
+    tile.visualElements.showNameOnSquare150x150Logo = true;
+    tile.visualElements.foregroundText = Windows.UI.StartScreen.ForegroundText.light;
+    tile.visualElements.square30x30Logo = square30x30Logo;
+
+    var selectionRect = element.getBoundingClientRect();
+    var buttonCoordinates = { x: selectionRect.left, y: selectionRect.top, width: selectionRect.width, height: selectionRect.height };
+    var placement = Windows.UI.Popups.Placement.above;
+
+    return new WinJS.Promise(function (complete, error, progress) {
+        tile.requestCreateForSelectionAsync(buttonCoordinates, placement).done(function (isCreated) {
+            if (isCreated) {
+                complete(true);
+            }
+            else {
+                complete(false);
+            }
+        });
+    });
+}
+
+function unpinByElementAsync(element, unwantedTileID) {
+    var selectionRect = element.getBoundingClientRect();
+    var buttonCoordinates = { x: selectionRect.left, y: selectionRect.top, width: selectionRect.width, height: selectionRect.height };
+    var placement = Windows.UI.Popups.Placement.above;
+
+    var tileToDelete = new Windows.UI.StartScreen.SecondaryTile(unwantedTileID);
+
+    return new WinJS.Promise(function (complete, error, progress) {
+        tileToDelete.requestDeleteForSelectionAsync(buttonCoordinates, placement).done(function (isDeleted) {
+            if (isDeleted) {
+                complete(true);
+            }
+            else {
+                complete(false);
+            }
+        });
+    });
+}
+
+function getAppbarTileId() {
+    var appbarTileId = appbarTileUri;
+    var uriLength = appbarTileUri.length;
+    if (uriLength > 64) {
+        appbarTileId = appbarTileId.substr(uriLength - 64, 64);
+    }
+
+    return appbarTileId.replace(new RegExp(/[\|\\"\/<>\?\;\:\!']/g), "-");
+}
+
+appbarTileUri = window.location.href;
+
+var loadingContent = document.createElement("div");
+loadingContent.className = "loadingContent";
+loadingContent.style.position = "absolute";
+loadingContent.style.left = 0;
+loadingContent.style.top = 0;
+loadingContent.style.zIndex = 2;
+loadingContent.style.width = "100%";
+loadingContent.style.height = "100%";
+loadingContent.style.textAlign = "center";
+loadingContent.style.verticalAlign = "middle";
+loadingContent.style.backgroundColor = "#3377FF";
+loadingContent.style.opacity = 0.3;
+
+var progress = document.createElement("progress");
+progress.classList.add("win-ring");
+progress.classList.add("win-large");
+progress.style.position = "absolute";
+progress.style.margin = "auto";
+progress.style.overflow = "auto";
+progress.style.left = 0;
+progress.style.top = 0;
+progress.style.right = 0;
+progress.style.bottom = 0;
+
+loadingContent.appendChild(progress);
+document.body.appendChild(loadingContent);
+
+setWinJS();
